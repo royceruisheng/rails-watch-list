71d8c6355e653cb6d82eff6a5ed0803bbb063964
royceruisheng <ruisheng.lim@gmail.com>
2021-09-10 13:04:33 +0800

    done the controller and view of lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'lists/index'
+  get 'lists/create'
+  get 'lists/new'
+  get 'lists/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, only: [:index, :show, :new, :create]
 end
